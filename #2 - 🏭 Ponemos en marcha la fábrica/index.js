f4125cb79b7ee0da58cafeb5ae538509e0603666
DaviidMM <davicitoo1612@gmail.com>
2023-12-03 11:47:00 +0100

    Add solution to #2 challenge

---
+++ #2 - 🏭 Ponemos en marcha la fábrica/index.js
@@ -0,0 +1,25 @@
+function manufacture(gifts, materials) {
+  return gifts.reduce((acc, gift) => {
+    const uniqueCharacters = [...new Set(gift)];
+    const canCraft = uniqueCharacters.every((char) => materials.includes(char));
+    if (canCraft) acc = [...acc, gift];
+    return acc;
+  }, []);
+}
+
+const gifts = ['tren', 'oso', 'pelota'];
+const materials = 'tronesa';
+
+console.log(manufacture(gifts, materials)); // ["tren", "oso"]
+// 'tren' SÍ porque sus letras están en 'tronesa'
+// 'oso' SÍ porque sus letras están en 'tronesa'
+// 'pelota' NO porque sus letras NO están en 'tronesa'
+
+const gifts2 = ['juego', 'puzzle'];
+const materials2 = 'jlepuz';
+
+console.log(manufacture(gifts2, materials2)); // ["puzzle"]
+const gifts3 = ['libro', 'ps5'];
+const materials3 = 'psli';
+
+console.log(manufacture(gifts3, materials3)); // []
